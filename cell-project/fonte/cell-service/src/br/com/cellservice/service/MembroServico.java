32bd915f78749562de1ef2fad882d3b1cbd89b6a
dryengalvao <dryenhanzo@gmail.com>
2015-06-22 20:02:41 -0400

    import do codigo do projeto

---
+++ cell-project/fonte/cell-service/src/br/com/cellservice/service/MembroServico.java
@@ -0,0 +1,18 @@
+package br.com.cellservice.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.codehaus.jettison.json.JSONArray;
+
+@Path("/membros")
+public class MembroServico {
+
+	@GET
+	@Produces("application/json")
+	public JSONArray lista() {
+		return null;
+	}
+
+}
